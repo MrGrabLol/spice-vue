d661902133fedaadfc08fc5fec1b3020fa0cd52e
Александр Богомолов <2002dalek@gmail.com>
2022-09-22 01:15:56 +0300

    commit add model not working blyat

--- src/router/index.js
+++ src/router/index.js
@@ -1,64 +1,65 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
     {
-        path: '/customer-page',
-        name: 'CP',
-        component: () => import('../pages/CustomerPage.vue'),
+        path: "/customer-page",
+        name: "CP",
+        component: () => import("../pages/CustomerPage.vue"),
         children: [
             {
-                path: 'about',
-                name: 'About',
-                component: () => import('../views/AboutView.vue')
+                path: "about",
+                name: "About",
+                component: () => import("../views/AboutView.vue"),
             },
             {
-                path: 'models',
-                name: 'Models',
-                component: () => import('../views/ModelsView.vue')
+                path: "models",
+                name: "Models",
+                component: () => import("../views/ModelsView.vue"),
             },
             {
-                path: 'order',
-                name: 'Order',
-                component: () => import('../views/OrderView.vue')
+                path: "order",
+                name: "Order",
+                component: () => import("../views/OrderView.vue"),
             },
             {
-                path: 'home',
-                name: 'Home',
-                component: () => import('../views/HomeView.vue')
-            }]
+                path: "home",
+                name: "Home",
+                component: () => import("../views/HomeView.vue"),
+            },
+        ],
     },
     {
-        path: '/admin-page',
-        name: 'Admin',
-        component: () => import('../views/adminPage/AdminView.vue'),
+        path: "/admin-page",
+        name: "Admin",
+        component: () => import("../views/adminPage/AdminView.vue"),
         children: [
             {
-                path: 'model',
-                name: 'AdminModels',
-                component: () => import('../views/adminPage/ModelView.vue'),
-                children: [
-                    {
-                        path: ':id',
-                        name: 'AdminModelEdit',
-                        component: () => import('../views/adminPage/ModelView.vue')
-                    }
-                ]
+                path: "model",
+                name: "AdminModels",
+                component: () => import("../views/adminPage/ModelView.vue"),
             },
+
             {
-                path: 'request',
-                name: 'AdminRequest',
-                component: () => import('../views/adminPage/RequestView.vue')
-            }
-        ]
-    }
-]
+                path: "edit",
+                name: "AdminModelEdit",
+                component: () => import("../views/adminPage/AddModelView.vue"),
+            },
+
+            {
+                path: "request",
+                name: "AdminRequest",
+                component: () => import("../views/adminPage/RequestView.vue"),
+            },
+        ],
+    },
+];
 
 const router = new VueRouter({
     routes,
-    mode: 'history'
-})
+    mode: "history",
+});
 
-export default router
+export default router;
